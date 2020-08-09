4bb8338719b4cfd0aebec6933ed589adb6e211a1
ilzamafif <ilzamafif27@gmail.com>
2020-08-09 19:07:15 +0700

    update

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "./script/component/";
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import main from "./script/view/main.js";
-
+//server default heroku
+app.listen(process.env.PORT || 8080);
 $(main);
